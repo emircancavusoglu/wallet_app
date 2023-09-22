a1600e8c11d21fe73d7495713a346b5b74f5d284
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2023-09-22 16:42:34 +0300

    FİX ERROR

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -30,55 +30,57 @@ class _HomeViewState extends State<HomeView> {
           Icon(Icons.notifications_none),
         ],
       ),
-      body: Column(
-        children: [
-          Card(
-            margin: const EdgeInsets.only(left: 20, top: 20,right: 20),
-            child: Container(
-              color: Colors.indigo,
-              width: 400,
-              height: 200,
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("245,446",style: TextStyle(fontSize: 25),),
-                  SizedBox(height: 10,),
-                  Text("Total Balance",style: TextStyle(fontSize: 12),),
-                ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Card(
+              margin: const EdgeInsets.only(left: 20, top: 20,right: 20),
+              child: Container(
+                color: Colors.indigo,
+                width: 400,
+                height: 200,
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("245,446",style: TextStyle(fontSize: 25),),
+                    SizedBox(height: 10,),
+                    Text("Total Balance",style: TextStyle(fontSize: 12),),
+                  ],
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 10,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton.icon(onPressed: (){}, icon: const Icon(Icons.price_change),
-                label: const Text("Top Up"),style: TextButton.styleFrom(backgroundColor: Colors.grey),
-              ),
-              const SizedBox(width: 10,),
-              ElevatedButton.icon(onPressed: (){}, icon: const Icon(Icons.currency_exchange_sharp),
-                label: const Text("Transfer"),style:
-                TextButton.styleFrom(backgroundColor: TextColor.subtitleColor),
-              ),
-            ],
-          ),
-          const SizedBox(height: 20,),
-          Container(
-            margin: const EdgeInsets.only(right: 280),
-            child: const Text("Transactions",style: TextStyle(fontSize: 17,color: TextColor.titleColor),
+            const SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(onPressed: (){}, icon: const Icon(Icons.price_change),
+                  label: const Text("Top Up"),style: TextButton.styleFrom(backgroundColor: Colors.grey),
+                ),
+                const SizedBox(width: 10,),
+                ElevatedButton.icon(onPressed: (){}, icon: const Icon(Icons.currency_exchange_sharp),
+                  label: const Text("Transfer"),style:
+                  TextButton.styleFrom(backgroundColor: TextColor.subtitleColor),
+                ),
+              ],
             ),
-          ),
-          const SizedBox(height: 20,),
-          const ProfileWidget(),
-          const Padding(padding: EdgeInsets.only(top: PaddingSizes.paddingTop)),
-          const ProfileWidget(),
-          const Padding(padding: EdgeInsets.only(top: PaddingSizes.paddingTop)),
-          const ProfileWidget(),
-          const Padding(padding: EdgeInsets.only(top: PaddingSizes.paddingTop)),
-          const ProfileWidget(),
-          const Padding(padding: EdgeInsets.only(top: PaddingSizes.paddingTop)),
-          const ProfileWidget(),
-        ],
+            const SizedBox(height: 20,),
+            Container(
+              margin: const EdgeInsets.only(right: 280),
+              child: const Text("Transactions",style: TextStyle(fontSize: 17,color: TextColor.titleColor),
+              ),
+            ),
+            const SizedBox(height: 20,),
+            const ProfileWidget(),
+            const Padding(padding: EdgeInsets.only(top: PaddingSizes.paddingTop)),
+            const ProfileWidget(),
+            const Padding(padding: EdgeInsets.only(top: PaddingSizes.paddingTop)),
+            const ProfileWidget(),
+            const Padding(padding: EdgeInsets.only(top: PaddingSizes.paddingTop)),
+            const ProfileWidget(),
+            const Padding(padding: EdgeInsets.only(top: PaddingSizes.paddingTop)),
+            const ProfileWidget(),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.white,
