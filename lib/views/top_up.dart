ff9cd73311d153c7923e1c7c04b68a5d3bc1bc7f
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2023-09-28 13:33:31 +0300

    fetchData

--- lib/views/top_up.dart
+++ lib/views/top_up.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:wallet_app/constants/http_path.dart';
@@ -10,6 +12,10 @@ class PostModelView extends StatefulWidget {
 }
 
 class _PostModelViewState extends State<PostModelView> {
+  bool isLoading = false;
+  void changeIsLoading(){
+    isLoading = !isLoading;
+  }
   @override
   void initState(){
     super.initState();
@@ -17,17 +23,33 @@ class _PostModelViewState extends State<PostModelView> {
   }
   Future<void>fetchData() async{
     final response = await Dio().get(ServicePaths.httpPath);
+    if(response.statusCode == HttpStatus.ok){
+      final _datas = response.data;
+
+      if(_datas is List){
+        setState(() {
+          _items = _datas.map((e) => PostModel.fromJson(e)).toList();
+        });
+      }
+    }
   }
   List<PostModel>? _items;
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          isLoading ? const CircularProgressIndicator(): SizedBox(),
+        ],
+      ),
       body: ListView.builder(
         itemCount: _items?. length ?? 0,
         itemBuilder: (context, index) {
-        return Text("_items");
+        return ListTile(
+          title: Text(_items?[index].title ?? ''),
+          subtitle: Text(_items?[index].body ?? ''),
+        );
       },),
     );
   }
