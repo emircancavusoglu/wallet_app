537fe5d10f43a83f0e5a80e6270ae2dc9472acfc
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2023-10-03 18:12:42 +0300

    add password feauture and create textField

--- lib/views/completed_page.dart
+++ lib/views/completed_page.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
 
-class CompletedView extends StatelessWidget {
+class CompletedView extends StatefulWidget {
   const CompletedView({Key? key}) : super(key: key);
 
+  @override
+  State<CompletedView> createState() => _CompletedViewState();
+}
+
+class _CompletedViewState extends State<CompletedView> {
+  final TextEditingController _passwordController = TextEditingController();
+  bool isSecure = true;
+  void changeSecure(){
+    setState(() {
+      isSecure = !isSecure;
+    });
+    }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Text("Completed"),
+      body: TextField(
+        autofillHints: const [AutofillHints.telephoneNumber],
+        obscureText: isSecure,
+        controller: _passwordController,
+        keyboardType: TextInputType.number,
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          border: const UnderlineInputBorder(),
+          label: const Text("Secure Code"),
+          hintText: '7 haneli güvenlik kodunuzu giriniz.',
+          suffixIcon: IconButton(onPressed: () {
+            changeSecure();
+          }, icon: Icon( isSecure ?  Icons.visibility : Icons.visibility_off)),
+        ),
+      ),
     );
   }
 }
