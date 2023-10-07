1a0fce35a416a35412297e62f972cd9bc7db7446
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2023-10-08 00:41:12 +0300

    cacheManagerClass

--- lib/cache/cache_manager.dart
+++ lib/cache/cache_manager.dart
@@ -1,1 +1,9 @@
 import 'package:shared_preferences/shared_preferences.dart';
+
+class CacheManager{
+  Future<void> saveString(String key, String value)async{}
+  Future<String?>getString(String key)async{}
+  Future<bool?> removeString(String key)async{}
+
+}
+
