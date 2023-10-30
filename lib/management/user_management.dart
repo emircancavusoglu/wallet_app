3c3e1ac14ab802016ef3efeca89e322d525fe972
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2023-10-30 20:01:29 +0300

    add SenderPerson class and fold

--- lib/management/user_management.dart
+++ lib/management/user_management.dart
@@ -4,10 +4,23 @@ class UserManagement{
     print(user.money);
   }
 }
-
+int calculateMoney(List<User>user){
+  int initialValue =0;
+  int sum = 0;
+  for(var item in user){
+    sum += item.money;
+  }
+  final sumMoney = user.fold(initialValue, (previousValue, element) => previousValue+element.money);
+  return sumMoney;
+}
 class User{
   final String name;
-  final int ibanNo;
+  final String ibanNo;
   final int money;
   User(this.name, this.ibanNo, this.money);
+}
+class SenderPerson extends User{
+  int role;
+  SenderPerson(String name, String ibanNo, int money, this.role) : super(name,ibanNo, money);
+
 }
